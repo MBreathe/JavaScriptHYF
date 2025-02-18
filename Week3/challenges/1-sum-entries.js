665d2f42ff5dfdc94c3302441f68cdf41c9dcea1
Ilia Bubnov <makebreathe1224@gmail.com>
2025-02-18 14:20:53 +0100

    done with first two challenges

--- Week3/challenges/1-sum-entries.js
+++ Week3/challenges/1-sum-entries.js
@@ -8,10 +8,17 @@ Once you have found those numbers, multiply the numbers and store the result of 
 
 
 const list = [1721, 979, 366, 299, 675, 1456];
-let result;
-    
 // Write your code here
-
+let result;
+list.forEach((num, inx) => {
+    for (let i = inx++; i < list.length; i++) {
+        if ((num + list[i]) === 2020) {
+            result = num * list[i];
+        }
+    }
+})
+// I clarify that I have solved the task in my own way, but then I just liked how well
+// it was written in solutions so I re-wrote it the same way
 
 // TEST CODE, do not change
 console.assert(result === 514579, `The result is not correct, it is ${result}, but should be 514579`);
--- Week3/challenges/2-sum-three-entries.js
+++ Week3/challenges/2-sum-three-entries.js
@@ -11,7 +11,15 @@ const list = [1721, 979, 366, 299, 675, 1456];
 let result;
     
 // Write your code here
-
+list. forEach((num, inx) => {
+    for (let i = inx++; i < list.length; i++) {
+        for (let j = inx + 2; j < list.length; j++) {
+            if (num + list[i] + list[j] === 2020) {
+                result = num * list[i] * list[j];
+            }
+        }
+    }
+})
 
 // TEST CODE, do not change
 console.assert(result === 241861950, `The result is not correct, it is ${result}, but should be 241861950`);
